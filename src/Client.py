5d151bca098b42c6dc6e2befb2eace3b393c2d4a
und <pierre-emmanuel.patry@epita.fr>
2019-03-26 03:33:36 +0100

    Add project architecture

---
+++ src/Client.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+__author__ = "Maxime-Andrea Gouet and Pierre-Emmanuel Patry"
+__copyright__ = "Copyright 2019, Maxime-Andrea Gouet & Pierre-Emmanuel Patry"
+__credits__ = ["Maxime-Andrea Gouet", "Pierre-Emmanuel Patry"]
+__license__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "Pierre-Emmanuel Patry"
+__email__ = "ppatry@csumb.edu"
+__status__ = "Production"
